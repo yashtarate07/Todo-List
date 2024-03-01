f97342b01e62152733e668f493ba4aa4bcbb3810
Yash <yash4545tarate@gmail.com>
2024-03-01 14:04:54 +0530

    intial commit

---
+++ front/src/components/Header/index.jsx
@@ -0,0 +1,30 @@
+
+import styles from './header.module.css';
+import { AiOutlinePlusCircle } from 'react-icons/ai';
+import { useState } from 'react';
+
+export function Header({ handleAddTask }) {
+  const [title, setTitle] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    handleAddTask(title);
+    setTitle('');
+  }
+
+  function onChangeTitle(event) {
+    setTitle(event.target.value);
+  }
+
+  return (
+    <header className={styles.header}>
+      <h1>Get Work Done!</h1>
+
+      <form onSubmit={handleSubmit} className={styles.newTaskForm}>
+        <input placeholder="Add a new task" type="text" onChange={onChangeTitle} value={title} />
+        <button>Create <AiOutlinePlusCircle size={20} /></button>
+      </form>
+    </header>
+  )
+}
